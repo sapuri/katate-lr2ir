334ca971110327b348fa5cd237a5d4d7fb8d1f05
voltilamm <voltilamm@icloud.com>
2018-05-25 11:57:28 +0900

    model test

--- mastermind/models.py
+++ mastermind/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Score(models.Model):
+    bms_id = models.CharField('BMSID', max_length=255)
+    player_name = models.CharField('プレイヤー名', max_length=255)
+    clear_type = models.CharField('クリア状況', max_length=255)
+    score_lank = models.CharField('スコアランク', max_length=255)
+    score = models.CharField('スコア', max_length=255)
+    combo = models.CharField('コンボ', max_length=255)
+    BP = models.CharField('BP', max_length=255)
+    PG = models.CharField('PG', max_length=255)
+    GR = models.CharField('GR', max_length=255)
+    GD = models.CharField('GD', max_length=255)
+    BD = models.CharField('BD', max_length=255)
+    PR = models.CharField('PR', max_length=255)
+
+    def __str__(self):
+        return self.bms_id
+
+    class Meta:
+        verbose_name = 'スコア'
+        verbose_name_plural = 'スコア'
