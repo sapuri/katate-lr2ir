8a77525efc3ad5a138219778bbbd61d0d1d877f6
voltilamm <voltilamm@icloud.com>
2018-05-28 10:59:43 +0900

    views test

--- mastermind/models.py
+++ mastermind/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Score(models.Model):
-    bms_id = models.CharField('BMSID', max_length=255)
-    player_name = models.CharField('プレイヤー名', max_length=255)
-    clear_type = models.CharField('クリア状況', max_length=255)
-    score_lank = models.CharField('スコアランク', max_length=255)
-    score = models.CharField('スコア', max_length=255)
-    combo = models.CharField('コンボ', max_length=255)
-    BP = models.CharField('BP', max_length=255)
-    PG = models.CharField('PG', max_length=255)
-    GR = models.CharField('GR', max_length=255)
-    GD = models.CharField('GD', max_length=255)
-    BD = models.CharField('BD', max_length=255)
-    PR = models.CharField('PR', max_length=255)
+    bms_id = models.CharField('BMSID', max_length=8)
+    player_name = models.CharField('プレイヤー名', max_length=16)
+    clear_type = models.CharField('クリア状況', max_length=8)
+    score_lank = models.CharField('スコアランク', max_length=8)
+    score = models.CharField('スコア', max_length=32)
+    combo = models.CharField('コンボ', max_length=16)
+    BP = models.CharField('BP', max_length=8)
+    PG = models.CharField('PG', max_length=8)
+    GR = models.CharField('GR', max_length=8)
+    GD = models.CharField('GD', max_length=8)
+    BD = models.CharField('BD', max_length=8)
+    PR = models.CharField('PR', max_length=8)
 
     def __str__(self):
         return self.bms_id
