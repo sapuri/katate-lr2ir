058345d8fa866774ef464f0e30c46fc4486ff1c2
voltilamm <voltilamm@icloud.com>
2018-05-31 10:46:54 +0900

    add bms_list page and tablesorter

--- ranking/views.py
+++ ranking/views.py
@@ -5,6 +5,10 @@ from mastermind.models import Player
 from mastermind.models import Bms
 from mastermind.models import Score
 
+def bms_list(request):
+    b = Bms.objects.all()
+    return render(request, 'ranking/bms_list.html', {'bms_list': b})
+
 def bms_ranking(request, bms_id):
     b = Bms.objects.filter(bms_id__contains=bms_id).first()
     s = Score.objects.filter(bms_id__contains=bms_id)
