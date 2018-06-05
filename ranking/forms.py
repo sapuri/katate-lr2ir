448f1295620c998d945ce1161ec7463a7d4c5d12
voltilamm <voltilamm@icloud.com>
2018-06-05 11:56:41 +0900

    apply form to bootstrap

--- ranking/forms.py
+++ ranking/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from mastermind.models import Player
 
 class PlayerDataForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PlayerDataForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
     class Meta:
         model = Player
         fields = ('player_id', 'name')
